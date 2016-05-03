8350150346ab55a86346cf0a343f18a51faf255b
Peter Smit <peter@smitmail.eu>
2016-05-03 22:48:26 +0300

    Initial GCC installation code

---
+++ gcc/create_prgenv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source ../common/common.sh
+
+VERSION=${1}
+NAME=prgenv
+
+init_vars
+
+write_module
---
+++ gcc/install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+source ../common/common.sh
+
+VERSION=${1:-6.1.0}
+NAME=gcc
+
+init_vars
+
+pushd ${OPT_DIR}
+
+rm -Rf gcc-${VERSION}* ${VERSION}
+
+wget ftp://ftp.nluug.nl/mirror/languages/gcc/releases/gcc-${VERSION}/gcc-${VERSION}.tar.gz || error_exit "Could not download this version"
+
+tar -zxf gcc-${VERSION}.tar.gz
+mkdir ${BUILD_DIR}
+pushd ${BUILD_DIR}
+./configure --prefix=${OPT_DIR}/${VERSION} --disable-multilib
+make
+make install
+
+export LIB_PATH=${OPT_DIR}/${VERSION}/lib64
+
+./create_prgenv.sh ${VERSION}
+
+BIN_PATH=${OPT_DIR}/${VERSION}/bin
+DESC="GCC compiler"
+HELP="GCC ${VERSION}"
+
+read -d '' EXTRA_LINES << EOF
+module load prgenv
+
+prepend-path CPATH ${OPT_DIR}/${VERSION}/include
+prepend-path LIBRARY_PATH ${OPT_DIR}/${VERSION}/lib64
+prepend-path MAN_PATH ${OPT_DIR}/${VERSION}/share/man
+EOF
+
+write_module
+
+popd
+rm -Rf gcc-${VERSION}*
+popd
