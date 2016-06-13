80005c2a81cc77ead912cfda5b8b0ce575ab43e1
Peter Smit <peter.smit@aalto.fi>
2016-06-13 13:40:45 +0300

    Update AaltoASR. Use rpath for libstdc++ and fix swig wrapper compilation

--- AaltoASR/install.sh
+++ AaltoASR/install.sh
@@ -8,7 +8,7 @@ GIT_BRANCH=develop
 
 init_vars
 
-module load gcc
+module load GCC
 module load cmake
 
 checkout_git
@@ -16,7 +16,7 @@ checkout_git
 mkdir ${BUILD_DIR}/build
 pushd ${BUILD_DIR}/build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_CXX_FLAGS="-Wl,-rpath,${EBROOTGCC}/lib64" ..
 make install
 
 popd
@@ -26,7 +26,6 @@ LIB_PATH=${INSTALL_DIR}/lib
 
 
 read -d '' EXTRA_LINES << EOF
-module load prgenv
 append-path     PYTHONPATH ${LIB_PATH}/site-packages
 append-path     PERL5LIB ${GIT_PATH}/aku/scripts
 append-path     PATH ${GIT_PATH}/pyrectool
