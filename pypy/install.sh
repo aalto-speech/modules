ff70de5c3ece1d1339a735b1e519fa24033d9c93
Peter Smit <peter.smit@aalto.fi>
2016-06-13 12:36:05 +0300

    Bump pypy version number

--- pypy/install.sh
+++ pypy/install.sh
@@ -4,18 +4,18 @@
 
 source ../common/common.sh
 
-VERSION=${1:-5.0.0}
+VERSION=${1:-5.3.0}
 NAME=pypy
 
 init_vars
 
 pushd ${OPT_DIR}
 
-wget https://bitbucket.org/pypy/pypy/downloads/pypy-${VERSION}-src.tar.bz2 || error_exit "Could not download this version"
+wget https://bitbucket.org/pypy/pypy/downloads/pypy2-v${VERSION}-src.tar.bz2 || error_exit "Could not download this version"
 
 mkdir -p ${OPT_DIR}/${VERSION}
 
-tar xjf pypy-${VERSION}-src.tar.bz2 --strip-components 1 -C ${OPT_DIR}/${VERSION}
+tar xjf pypy2-v${VERSION}-src.tar.bz2 --strip-components 1 -C ${OPT_DIR}/${VERSION}
 pushd ${OPT_DIR}/${VERSION}
 
 python2 rpython/bin/rpython -Ojit pypy/goal/targetpypystandalone.py
